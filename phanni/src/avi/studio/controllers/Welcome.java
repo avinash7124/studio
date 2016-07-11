cd459239d5d65a876da317d28f6d756e5fffa5e7
aviSingh <aviSingh@aviSingh-PC>
2016-07-12 02:41:52 +0530

    ..

--- phanni/src/avi/studio/controllers/Welcome.java
+++ phanni/src/avi/studio/controllers/Welcome.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping(value = "/index")
+@RequestMapping(value = "/")
 public class Welcome {
 
 	@RequestMapping(method = RequestMethod.GET)
